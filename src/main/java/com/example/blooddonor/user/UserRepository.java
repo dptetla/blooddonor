c087dec36fe5d20af49396e6081063ca09bb0145
daniel <dptetla@gmail.com>
2022-07-15 00:44:16 +0200

    Projekt 1

---
+++ src/main/java/com/example/blooddonor/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.blooddonor.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+        User findByUserName(String username);
+        User findByEmail (String email);
+
+}
